1487fd6e6c0a143ee600d359f740b84bac2bdc0f
Naisa <naisa@mail.ru>
2023-02-11 02:07:01 +0300

    шаблон карусель подключено

--- django_talantCity/django_talantCity/settings.py
+++ django_talantCity/django_talantCity/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'sam',
 ]
 
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'django_talantCity.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = BASE_DIR / 'static'
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- django_talantCity/sam/views.py
+++ django_talantCity/sam/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import *
 
-# Create your views here.
+
+class TrainingsView(View):
+    def get(self, request):
+        trainings = Trainings.objects.all()
+        return render(request, "sam/index.html", {"training_list": trainings, "trainings_count": range(trainings.count())})
+
